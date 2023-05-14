8563346b18c57c0e722c00265d5a5e83ae572988
Sharon Shaliyo <sharon.shaliyo@gmail.com>
2023-05-14 07:37:37 +0530

    feat: Update components with MUI

--- src/components/SimpleTodo/SimpleTodo.js
+++ src/components/SimpleTodo/SimpleTodo.js
@@ -1,25 +1,49 @@
 import React, { useState } from "react";
-import SimpleTodoItem from './SimpleTodoItem'
-import { Button } from "@mui/material";
+import { Box, Button, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
+import SimpleTodoItem from './SimpleTodoItem'
 
 export default () => {
     const [todos, setTodos] = useState(['Learn', 'Grind'])
     return <>
         <div>
-            <h3>Todo</h3>
-            <form>
-                <input type="text" placeholder="Title" />
+            <Typography variant="h5" style={{ fontWeight: 600 }} sx={{ mb: 3, mt: 3 }}>
+                Cross-functional project plan
+            </Typography>
+            <Stack spacing={2} direction="row" sx={{ mb: 4 }}>
+                <TextField
+                    type="text"
+                    label="Title"
+                    className="full-width-input"
+                    size="small"
+                />
+                <TextField
+                    className="full-width-input"
+                    type="text"
+                    label="Description"
+                    size="small"
+                />
                 <Button
                     variant="contained"
                     onClick={null}
                     size="small"
                     startIcon={<AddIcon />}
                 >Add</Button>
-            </form>
-            <ul>
-                {todos.map(todo => <SimpleTodoItem todo={todo} />)}
-            </ul>
+            </Stack>
+            <Box>
+                <TableContainer>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Task</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {todos.map(todo => <SimpleTodoItem todo={todo} />)}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Box>
         </div>
     </>
 }
--- src/components/SimpleTodo/SimpleTodoItem.js
+++ src/components/SimpleTodo/SimpleTodoItem.js
@@ -1,10 +1,12 @@
+import { Checkbox, TableCell, TableRow } from '@mui/material'
 import React from 'react'
 
 const SimpleTodoItem = () => {
-    return (<li>
-        <input type="checkbox" />
-        <label>Todo Title</label>
-    </li>)
+    return (<TableRow className='task-header'>
+        <TableCell>
+            <Checkbox checked /> Todo Title
+        </TableCell>
+    </TableRow>)
 }
 
 export default SimpleTodoItem
