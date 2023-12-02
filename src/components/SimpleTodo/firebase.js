88e421b4984a0cb60edda195626eba443cf4564f
Sharon Shaliyo <sharon.shaliyo@gmail.com>
2023-12-02 22:48:11 +0530

    fix - update details

--- src/components/SimpleTodo/firebase.js
+++ src/components/SimpleTodo/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore"
 
 // Web app Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBgNZqW5uFxDV0ThdBY9aisZBImLIfpUC8",
-  authDomain: "ss-test1-1b22d.firebaseapp.com",
-  projectId: "ss-test1-1b22d",
-  storageBucket: "ss-test1-1b22d.appspot.com",
-  messagingSenderId: "580139249610",
-  appId: "1:580139249610:web:31e05708d97d12457de933"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
